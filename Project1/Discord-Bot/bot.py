5e3a530604c465f2993f17b954fd6ce1c47c6c76
CarsonRange <77698947+CarsonRange@users.noreply.github.com>
2022-01-25 14:54:57 -0500

    Update bot.py

--- Project1/Discord-Bot/bot.py
+++ Project1/Discord-Bot/bot.py
@@ -49,6 +49,8 @@ async def on_message(message):
         'Mimosa is literally orihime',
         'Hi Ben jojolion isnt coming out till the 19th, feelsbadman\n Wait part 6 ended this month?\n part 6 ended in 2005',
         'hi guys\n Why\n https://tenor.com/view/xqc-arabfunny-arabic-saudi-arabia-projared-gif-18306091',
+    ]    
+        
     if message.content == 'quote':
         #response = random.choice(brooklyn_99_quotes)
         response = random.choice(epic_quotes)
